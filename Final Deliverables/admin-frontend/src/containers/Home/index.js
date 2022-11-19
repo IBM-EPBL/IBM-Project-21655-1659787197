5979fc0c6321449c7803ea6099c6139d3a4114e6
manikandan190 <64845235+manikandan190@users.noreply.github.com>
2022-11-19 21:45:00 +0530

    Add files via upload

---
+++ Final Deliverables/admin-frontend/src/containers/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Jumbotron,Row,Col,Container } from 'react-bootstrap';
+import Layout from '../../components/Layout';
+import './style.css';   
+import { NavLink } from 'react-router-dom';
+
+const Home=(props)=>{
+
+
+
+   
+    return(
+        <Layout sidebar>
+       
+
+
+    
+        </Layout>
+    )
+}
+
+export default Home;
